cda02fe892e8941c10035866ae7ee9aa3800dbbd
hikky08 <57909305+hikky08@users.noreply.github.com>
2020-06-09 00:28:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+ const hamburger = document.querySelector('.hamburger');
+ const navLinks = document.querySelector('.nav-links');
+ const links = document.querySelectorAll('.nav-links li ');
+
+
+ hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+ });
+ 
+ 
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
